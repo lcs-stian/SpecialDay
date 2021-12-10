a0ff0a53e6472a6c3351843d0c1e078ffe8db603
Suzanne <STian@lcs.on.ca>
2021-12-10 08:55:31 -0500

    Initial Commit

---
+++ SpecialDay/SpecialDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpecialDayApp.swift
+//  SpecialDay
+//
+//  Created by Suzanne Tian on 2021-12-10.
+//
+
+import SwiftUI
+
+@main
+struct SpecialDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
